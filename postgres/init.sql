bda1c1fafc856c07d487fd13262b2e248352ee4f
Kanailal Manna <kanailalmanna2003@gmail.com>
2025-03-04 23:44:57 +0530

    Add Post and User models with relationships; update init.sql to include name column in users table

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     hashed_password VARCHAR(255) NOT NULL
 );
