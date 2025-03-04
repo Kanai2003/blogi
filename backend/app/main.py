691947c654a7940f421020cf5799c5e0d67c186f
Kanailal Manna <kanailalmanna2003@gmail.com>
2025-03-04 23:12:41 +0530

    Add db config and base running

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from .database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Blogi API is running"}
